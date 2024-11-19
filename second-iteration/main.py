bb7bad5c96dc3f40e59b3a3ccb070e9f29800b23
Vemund Ellingsson Røe <vemunderoe@gmail.com>
2024-11-19 04:02:54 +0100

    feat: Remove unnecessary samples and fix training files

--- second-iteration/main.py
+++ second-iteration/main.py
@@ -3,10 +3,8 @@ import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-from second_unet import UNet
-from enhanced_unet import EnhancedUNet
-from loss import ELBOLoss
-from second_dffusion import DiffusionModel
+from unet import UNet
+from diffusion import DiffusionModel
 from utils import visualize_noising_process, visualize_denoising_process
 from beta_scheduler import BetaScheduler
 import matplotlib.pyplot as plt
@@ -30,7 +28,7 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5,), (0.5,))  # Normalize to range [-1, 1]
 ])
-train_dataset = datasets.MNIST(root='./', train=True, transform=transform, download=True)
+train_dataset = datasets.MNIST(root='../data', train=True, transform=transform, download=True)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
 # Load the CIFAR-10 dataset
 # transform = transforms.Compose([
@@ -41,26 +39,13 @@ train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, nu
 # ])
 # train_dataset = datasets.CIFAR10(root='./data', train=True, transform=transform, download=True)
 # train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-# Load the CelebA dataset
-# transform = transforms.Compose([    
-#     transforms.Resize((64, 64)),  # Resize to 64x64
-#     transforms.ToTensor(),
-#     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize RGB channels to range [-1, 1]
-# ])
-# train_dataset = datasets.CelebA(root='./data', split="test", transform=transform, download=True)
-# train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-
 
 # Initialize the U-Net model, diffusion model, and loss function
-model = UNet(in_channels=1, out_channels=1, base_channels=64, embedding_dim=128).to(device)
-#model = UNet(in_channels=3, out_channels=3, base_channels=128, embedding_dim=128).to(device)
-
-print(f"Number of classes: {len(train_dataset.classes)}")
-#model = EnhancedUNet(in_channels=3, out_channels=3, base_channels=128, embedding_dim=128, num_classes=len(train_dataset.classes)).to(device)
+model = UNet(in_channels=1, out_channels=1, base_channels=64, embedding_dim=128).to(device)    # MNIST
+#model = UNet(in_channels=3, out_channels=3, base_channels=128, embedding_dim=128).to(device)  # CIFAR-10
 
 diffusion = DiffusionModel(model, beta_scheduler, num_timesteps=num_timesteps)
-#elboCriterion = ELBOLoss(beta_scheduler).to(device)
-mseCriterion = torch.nn.MSELoss().to(device) # use MSE loss for simplicity
+mseCriterion = torch.nn.MSELoss().to(device) 
 
 # Define the optimizer
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -70,7 +55,7 @@ print(f"Started at: {os.popen('date').read()}")
 visualize_noising_process(train_dataset[0][0].unsqueeze(0), diffusion, steps=10, path="./", filename="linear_noising", image_title="Linear Noising Visualization")
 #visualize_noising_process(train_dataset[0][0].unsqueeze(0), diffusion, steps=10, path="./", filename="cosine_noising", image_title="Cosine Noising Visualization")
 
-os.makedirs("checkpoints/mnist-linear-second", exist_ok=True)
+os.makedirs("checkpoints/mnist-linear", exist_ok=True)
 
 # Training loop
 epoch_avg_losses = []
@@ -78,9 +63,7 @@ for epoch in range(num_epochs):
     model.train()
     epoch_loss = 0
     for batch_idx, (x, labels) in enumerate(train_loader):
-        x = x.to(device)
-        #labels = labels.to(device)
-        #labels = None
+        x = x.to(device)        
 
         # Generate noise and timesteps
         noise = torch.randn_like(x)
@@ -89,16 +72,10 @@ for epoch in range(num_epochs):
         # Create noisy inputs
         noisy_x = diffusion.add_noise(x, noise, timesteps)
 
-        # if np.random.random() < 0.1:
-        #     labels = None
-
         # Predict the noise using the U-Net model (with timestep embeddings)
-        #predicted_noise = model(noisy_x, timesteps, None)
-        #predicted_noise = model(noisy_x, timesteps, labels)
         predicted_noise = model(noisy_x, timesteps)
 
         # Compute the loss
-        #loss = elboCriterion(predicted_noise, noise, timesteps)
         loss = mseCriterion(predicted_noise, noise)
 
         # Backpropagation and optimization
@@ -115,25 +92,20 @@ for epoch in range(num_epochs):
     epoch_avg_losses.append((epoch, avg_loss))
 
     # Save checkpoint
-    torch.save(model.state_dict(), f"checkpoints/mnist-linear-second/model_checkpoint_epoch_{epoch+1}.pth")    
+    torch.save(model.state_dict(), f"checkpoints/mnist-linear/model_checkpoint_epoch_{epoch+1}.pth")    
     print(f"Epoch {epoch + 1}: {os.popen('date').read()}")
 
     # Visualize the denoising process at the end of each epoch
     model.eval()
     with torch.no_grad():        
-        # Generate a sample and save show the denoising process
-        single_label = None
-        if labels is not None:
-            single_label = labels[0:1]        
-        generated_samples = diffusion.sample_ddpm(x_shape=(1, 1, 32, 32), device=device)               
-        #generated_samples = diffusion.sample_ddpm(x_shape=(1, 1, 32, 32), device=device, labels=single_label)                
-        #generated_samples = diffusion.sample_ddpm(x_shape=(1, 3, 32, 32), device=device)                
-        #generated_samples = diffusion.sample_ddpm(x_shape=(1, 3, 64, 64), device=device)                
-        visualize_denoising_process(generated_samples, epoch=epoch, path="samples/generated_samples/mnist-linear-second", filename="samples_epoch", image_title="Denoising Process Visualization")
+        # Generate a sample and save show the denoising process             
+        generated_samples = diffusion.sample(x_shape=(1, 1, 32, 32), device=device)  # MNIST   
+        #generated_samples = diffusion.sample(x_shape=(1, 3, 32, 32), device=device) # CIFAR-10
+        visualize_denoising_process(generated_samples, epoch=epoch, path="generated_samples/mnist-linear", filename="samples_epoch", image_title="Denoising Process Visualization")
 
 
 # Save the trained model
-torch.save(model.state_dict(), "checkpoints/mnist-linear-second/model.pth")
+torch.save(model.state_dict(), "checkpoints/mnist-linear/model.pth")
 print("Training completed and model saved.")
 
 # Save the epoch losses to a file
